d12c6673d6d06217051fc4e2d1b5b319f74e8a47
Omar Ahmad <theomar3@gmail.com>
2016-09-09 09:34:23 -0500

    final update

--- public/practice.js
+++ public/practice.js
@@ -3,76 +3,80 @@ if (this.AppName === undefined) this.AppName = {};
 
 (function(context) {
 
-  var $inputValue = $('#item-input').val();
+  var $itemInputValue = $('#item-input').val();
   var $list = $('.list');
 
-
-  function templateEntry(text, taskId, isComplete) {
+  var templateEntry(text, id, isComplete) {
     var className = '';
     if(isComplete == 'true') {
       className = 'item-done';
     }
 
-    var $templateHtml = $('#todo-template').html();
-    var templateFunc = _.template($templateHtml);
-    var html = templateFunc(
-      {
-        text: text,
-        taskId: taskId,
-        className: className
-
-      }
-    );
+    var templateHtml = $('#todo-template').html();
+    var templateFunc = _.template(templateHtml);
+    var html = templateFunc({
+      text: $itemInputValue,
+      id: id,
+      className: className
+    });
     $list.append(html);
-    $inputValue = '';
   }
 
-  function postAPIDataAndList() {
+
+  function getAPIData() {
+
 
     var promise = $.ajax({
-      url: '/api/todo',
+      url: 'api/todo',
       method: 'POST',
       data: {
-        text: $inputValue,
+        text: $itemInputValue,
         isComplete: false
       }
     });
+
     promise.done(apiResponse);
 
     function apiResponse(data) {
-      console.log(data);
-      templateEntry($inputValue, data.taskId, false);
+      templateEntry($itemInputValue, data.id, false);
+    }
+    $itemInputValue = '';
+  }
+
+  function keyUpHappened(evt) {
+    if(evt.keyCode === 13) {
+      getAPIData();
     }
 
   }
 
-  function getAPIData() {
+  function loadExistingData() {
     var promise = $.ajax({
-      url: '/api/todo',
+      url: 'api/todo',
     });
-    promise.done(gotAPIData);
 
-    function gotAPIData(data) {
-      console.log(data);
-      data.list.forEach(entries);
-    }
+    promise.done(gotAPIDataBack);
 
-    function entries(entry) {
-      console.log(entry);
-      templateEntry(entry.text, entry.taskId, entry.isComplete);
+    function gotAPIDataBack(data) {
+      data.list.forEach(function(response) {
+        templateEntry(response.text, response.id, response.isComplete);
+      });
     }
   }
 
   function deleteItem(evt) {
+    evt.stopPropagation();
     var $target = $(evt.target);
     var id = $target.parent().data('id');
 
-    $.ajax({
-      url: '/api/todo' + id,
+    var promise = $.ajax({
+      url: 'api/todo/' + id,
       method: 'DELETE'
     });
 
-    $target.parent().remove();
+    promise.done(function() {
+      $target.parent().remove();
+    });
   }
 
   function doneItem(evt) {
@@ -81,36 +85,26 @@ if (this.AppName === undefined) this.AppName = {};
     var text = $target.data('text');
 
     $.ajax({
-      url: '/api/todo' + id,
+      url: 'api/todo/' + id,
       method: 'PUT',
       data: {
         text: text,
-        taskId: id,
         isComplete: true
       }
     });
 
-    $target.parent().addClass('item-done');
-  }
-
-
-  function keyUpHappened(evt) {
-    if(evt.keyCode === 13) {
-      postAPIDataAndList();
-    }
+    $target.addClass('item-done');
   }
 
   function start() {
+    var $itemInput = $('#item-input');
+    $itemInput.on('keyup', keyUpHappened);
 
-    var $input = $('#item-input');
-    $input.on('keyup', keyUpHappened);
+    //when refreshed
+    loadExistingData();
 
-    //when page is refreshed
-    getAPIData();
-
-    $list.on('click', 'delete-button', deleteItem);
+    $list.on('click','delete-button', deleteItem);
     $list.on('click', doneItem);
-
   }
 
   context.start = start;
